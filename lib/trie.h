76444838e84d8d0645cd8dbed8e9e64b8f935a87
Ilia Shishkov <shishkov.ilia@gmail.com>
2024-08-29 16:04:35 +0300

    All tests OK

--- lib/trie.h
+++ lib/trie.h
@@ -12,17 +12,78 @@ concept StringLike = requires(const T& t) {
 
 class Trie {
 public:
-  Trie() {}
+  Trie() : nodes_(1) {}
 
   template <typename Container>
     requires requires(Container& c) {
       std::begin(c);
       std::end(c);
     }
-  explicit Trie(const Container& words) {}
+  explicit Trie(const Container& words) : Trie() {
+    for (auto&& w : words) {
+      Insert(w);
+    }
+  }
 
-  void Insert(const StringLike auto& word) {}
-  void Remove(const StringLike auto& word) {}
+  void Insert(const char* word) { Insert(std::string_view(word)); }
 
-  void Gather(const StringLike auto& prefix, std::regular_invocable<std::string_view> auto callback) const {}
+  void Insert(const StringLike auto& word) {
+    size_t node = 0;
+    for (char c : word) {
+      if (!nodes_[node].children_.contains(c)) {
+        nodes_[node].children_[c] = nodes_.size();
+        nodes_.emplace_back();
+      }
+      node = nodes_[node].children_[c];
+    }
+    nodes_[node].terminal_ = true;
+  }
+
+  void Remove(const char* word) { Remove(std::string_view(word)); }
+  
+  void Remove(const StringLike auto& word) {
+    size_t node = 0;
+    for (char c : word) {
+      if (!nodes_[node].children_.contains(c)) {
+        return;
+      }
+      node = nodes_[node].children_[c];
+    }
+    nodes_[node].terminal_ = false;
+  }
+
+  void Gather(const StringLike auto& prefix, std::regular_invocable<std::string_view> auto callback) const {
+    size_t node = 0;
+    for (char c : prefix) {
+      auto& children = nodes_[node].children_;
+      if (auto it = children.find(c); it == children.end()) {
+        return;
+      } else {
+        node = it->second;
+      }
+    }
+
+    std::string str(prefix);
+    DoGather(node, str, callback);
+  }
+
+private:
+  void DoGather(size_t node, std::string& cur_prefix, std::regular_invocable<std::string_view> auto callback) const {
+    if (nodes_[node].terminal_) {
+      callback(cur_prefix);
+    }
+
+    for (const auto& [c, child] : nodes_[node].children_) {
+      cur_prefix.push_back(c);
+      DoGather(child, cur_prefix, callback);
+      cur_prefix.pop_back();
+    }
+  }
+
+  struct Node {
+    bool terminal_ = false;
+    std::unordered_map<char, size_t> children_;
+  };
+
+  std::vector<Node> nodes_;
 };
--- tests/test_trie.cpp
+++ tests/test_trie.cpp
@@ -1,13 +1,15 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <string_view>
 
 #include "trie.h"
 
 namespace {
-std::vector<std::string_view> Collect(const Trie& trie, std::string_view prefix) {
-  std::vector<std::string_view> result;
-  trie.Gather(prefix, [&](std::string_view word) { result.push_back(word); });
+std::vector<std::string> Collect(const Trie& trie, std::string_view prefix) {
+  std::vector<std::string> result;
+  trie.Gather(prefix, [&](std::string_view word) { result.emplace_back(word); });
+  std::ranges::sort(result);
   return result;
 }
 }  // namespace
@@ -21,33 +23,33 @@ TEST(TrieTest, InsertAndGather) {
   trie.Insert("cherry");
 
   // Verify the gathered words
-  const std::vector<std::string_view> expectedWords = {"apple"};
+  const std::vector<std::string> expectedWords = {"apple"};
   EXPECT_EQ(Collect(trie, "a"), expectedWords);
 }
 
 TEST(TrieTest, PrefixSearch) {
   const Trie trie(std::vector{"a", "ab", "abc", "abcd", "bbb"});
 
-  EXPECT_EQ(Collect(trie, ""), std::vector<std::string_view>({"a", "ab", "abc", "abcd", "bbb"}));
-  EXPECT_EQ(Collect(trie, "a"), std::vector<std::string_view>({"a", "ab", "abc", "abcd"}));
-  EXPECT_EQ(Collect(trie, "ab"), std::vector<std::string_view>({"ab", "abc", "abcd"}));
-  EXPECT_EQ(Collect(trie, "abc"), std::vector<std::string_view>({"abc", "abcd"}));
-  EXPECT_EQ(Collect(trie, "abcd"), std::vector<std::string_view>({"abcd"}));
-  EXPECT_EQ(Collect(trie, "abcde"), std::vector<std::string_view>());
+  EXPECT_EQ(Collect(trie, ""), std::vector<std::string>({"a", "ab", "abc", "abcd", "bbb"}));
+  EXPECT_EQ(Collect(trie, "a"), std::vector<std::string>({"a", "ab", "abc", "abcd"}));
+  EXPECT_EQ(Collect(trie, "ab"), std::vector<std::string>({"ab", "abc", "abcd"}));
+  EXPECT_EQ(Collect(trie, "abc"), std::vector<std::string>({"abc", "abcd"}));
+  EXPECT_EQ(Collect(trie, "abcd"), std::vector<std::string>({"abcd"}));
+  EXPECT_EQ(Collect(trie, "abcde"), std::vector<std::string>());
 }
 
 TEST(TrieTest, Remove) {
   Trie trie(std::vector{"a", "ab", "abc", "abcd", "bbb"});
 
   trie.Remove("ab");
-  EXPECT_EQ(Collect(trie, ""), std::vector<std::string_view>({"a", "abc", "abcd", "bbb"}));
-  EXPECT_EQ(Collect(trie, "a"), std::vector<std::string_view>({"a", "abc", "abcd"}));
-  EXPECT_EQ(Collect(trie, "ab"), std::vector<std::string_view>({"abc", "abcd"}));
+  EXPECT_EQ(Collect(trie, ""), std::vector<std::string>({"a", "abc", "abcd", "bbb"}));
+  EXPECT_EQ(Collect(trie, "a"), std::vector<std::string>({"a", "abc", "abcd"}));
+  EXPECT_EQ(Collect(trie, "ab"), std::vector<std::string>({"abc", "abcd"}));
 }
 
 TEST(TrieTest, MoveConstructor) {
   Trie trie(std::vector{"a", "ab", "abc", "abcd", "bbb"});
   Trie newTrie(std::move(trie));
 
-  EXPECT_EQ(Collect(newTrie, ""), std::vector<std::string_view>({"a", "ab", "abc", "abcd", "bbb"}));
+  EXPECT_EQ(Collect(newTrie, ""), std::vector<std::string>({"a", "ab", "abc", "abcd", "bbb"}));
 }
